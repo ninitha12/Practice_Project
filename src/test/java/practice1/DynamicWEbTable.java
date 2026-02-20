152bb615139efb34d61fc6bcd08af42484c53395
ninitha12 <ninithasharon@gmail.com>
2026-02-20 15:26:41 +0530

    ho

--- src/test/java/practice1/DynamicWEbTable.java
+++ src/test/java/practice1/DynamicWEbTable.java
@@ -21,5 +21,8 @@ public static void main(String[] args) throws InterruptedException {
 	System.out.println("the "+name+" percent is: "+cpu);
 	Thread.sleep(3000);
 	driver.quit();
+	System.out.println("hi");
+	System.out.println("hi");
+
 }
 }
