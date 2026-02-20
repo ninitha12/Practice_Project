7bf47962be69298be304d3aac2ef2ba22f2112e3
ninitha12 <ninithasharon@gmail.com>
2026-02-20 13:32:01 +0530

    dynamic webtable

---
+++ src/test/java/practice1/DynamicWEbTable.java
@@ -0,0 +1,25 @@
+package practice1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DynamicWEbTable {
+public static void main(String[] args) throws InterruptedException {
+	WebDriver driver =new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	String name="System";
+	driver.get("https://testautomationpractice.blogspot.com/");
+	Actions act = new Actions(driver);
+	act.moveToElement(driver.findElement(By.id("apple"))).perform();
+	Thread.sleep(3000);
+	String cpu=driver.findElement(By.xpath("//tbody/tr/td[.='"+name+"']/following-sibling::td[contains(.,'%')]")).getText();
+	System.out.println("the "+name+" percent is: "+cpu);
+	Thread.sleep(3000);
+	driver.quit();
+}
+}
--- src/test/java/practice1/Page.java
+++ src/test/java/practice1/Page.java
@@ -51,5 +51,4 @@ public class Page {
 		driver.quit();
 
 	}
-
-}
+	}}
